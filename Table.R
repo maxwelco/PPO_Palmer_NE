7134a9a79c2db90e298b7c358e3dd207695c0897
Maxwel Coura Oliveira <36719666+maxwelco@users.noreply.github.com>
2020-05-21 17:58:14 -0300

    Add files via upload

---
+++ Table.R
@@ -0,0 +1,24 @@
+% Please add the following required packages to your document preamble:
+  % \usepackage{booktabs}
+% \usepackage{multirow}
+\begin{table}[]
+\caption{Monthly precipitation and mean air temperatures during 2013, 2014 and January through May 2015 compared to 30 year average at the Arlington Agriculture Research Station, Arlington, Wisconsin}
+\label{tab:my-table}
+\begin{tabular}{@{}lllllllll@{}}
+\toprule
+\multicolumn{1}{c}{\multirow{2}{*}{\textbf{Month}}} & \multicolumn{4}{c}{\textbf{Total Precipitation}}                                                                    & \multicolumn{4}{c}{\textbf{Mean Air Temperature}}                                               \\ \cmidrule(l){2-9} 
+\multicolumn{1}{c}{}                                & \multicolumn{1}{c}{\begin{tabular}[c]{@{}c@{}}30 yr\\ Normal\end{tabular}} & \multicolumn{1}{c}{2013} & 2014 & 2015 & \multicolumn{1}{c}{\begin{tabular}[c]{@{}c@{}}30 yr\\ Normal\end{tabular}} & 2013 & 2014 & 2015 \\ \midrule
+\multicolumn{1}{c}{}                                & \multicolumn{4}{c}{---- mm ----}                                                                                    & \multicolumn{4}{c}{---- C ----}                                                                 \\
+January                                             & 29                                                                         & 49                       & 4    & 5    & -9                                                                         & -7   & -13  & -8   \\
+February                                            & 33                                                                         & 33                       & 15   & 2    & -6.5                                                                       & -7   & -13  & -11  \\
+March                                               & 48                                                                         & 51                       & 23   & 13   & -0.3                                                                       & -4   & -4   & 0.4  \\
+April                                               & 89                                                                         & 154                      & 172  & 168  & 7                                                                          & 5    & 6    & 8    \\
+May                                                 & 94                                                                         & 158                      & 59   & 125  & 13                                                                         & 14   & 14   & 15   \\
+June                                                & 119                                                                        & 189                      & 238  &      & 19                                                                         & 24   & 26   &      \\
+August                                              & 99                                                                         & 42                       & 65   &      & 20                                                                         & 26   & 26   &      \\
+September                                           & 90                                                                         & 0.25                     & 31   &      & 15                                                                         & 25   & 21   &      \\
+October                                             & 65                                                                         & 50                       & 65   &      & 9                                                                          & 9    & 8    &      \\
+November                                            & 61                                                                         & 57                       & 32   &      & 1                                                                          & 0.35 & -3   &      \\
+December                                            & 37                                                                         & 11                       & 33   &      & -6                                                                         & -9   & -3   &      \\ \bottomrule
+\end{tabular}
+\end{table}
